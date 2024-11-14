f3b53fedc95c02f9d0125ba3afd2727077203395
Michael Kleen <mkleen@gmail.com>
2024-11-14 13:49:27 +0100

    Fix typo

--- server/src/main/java/io/crate/auth/CachingJwkProvider.java
+++ server/src/main/java/io/crate/auth/CachingJwkProvider.java
@@ -53,7 +53,7 @@ import io.netty.handler.codec.http.HttpHeaderNames;
  * Custom @{@link JwkProvider} implementation based on
  * <a href="https://github.com/auth0/jwks-rsa-java/blob/master/src/main/java/com/auth0/jwk/UrlJwkProvider.java">UrlJwkProvider.java</a>
  * which caches results of public jwk keys for the duration of the "Cache-Control max-age"
- * Http header valye from the response of the jwt authentication endpoint or
+ * Http header value from the response of the jwt authentication endpoint or
  * a provided default value.
  */
 public class CachingJwkProvider implements JwkProvider {
@@ -177,7 +177,7 @@ public class CachingJwkProvider implements JwkProvider {
         if (jwks.size() == 1) {
             return new JwkResult(jwks.get(0), ttl);
         }
-        
+
         for (Jwk jwk : jwks) {
             if (keyId.equals(jwk.getId())) {
                 return new JwkResult(jwk, ttl);
